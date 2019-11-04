65eda926a7b97946736c20a095cc20631c376763
Jonathan Moallem <jdmoallem@gmail.com>
2019-11-04 18:24:06 +1100

    Added new report type and renamed them for correlation matrix

--- main.py
+++ main.py
@@ -4,34 +4,33 @@ from scripts import exploration as exp
 import pandas as pd
 
 
-# Import the data and read from Excel file
-df = pd.read_csv('gameplay_report.csv')
+# Import the data and read from CSV file
+event_df = pd.read_csv('game_event_report.csv')
+param_df = pd.read_csv('game_param_report.csv')
 
 
-df['event_key'] = df.entity_type.map(str) + '_' + df.event_type
+event_df['event_key'] = event_df.entity_type.map(str) + '_' + event_df.event_type
 
 
-binarised_event_keys = prep.binarise(df.event_key)
+# binarised_event_keys = prep.binarise(event_df.event_key)
+#
+# processed_event_df = pd.DataFrame()
+# for event_key in binarised_event_keys.keys():
+#     processed_event_df[event_key] = binarised_event_keys[event_key]
+#
+# concatenated_df = pd.concat([event_df, processed_event_df], axis=1, sort=False)
 
-processed_df = pd.DataFrame()
-for event_key in binarised_event_keys.keys():
-    processed_df[event_key] = binarised_event_keys[event_key]
+# concatenated_df.drop('id', 1)
+# exp.correlation_matrix(concatenated_df, 'Correlation Matrix')
 
-concatenated_df = pd.concat([df, processed_df], axis=1, sort=False)
+exp.correlation_matrix(param_df, 'Correlation Matrix')
 
-
-
-exp.correlation_matrix(concatenated_df, 'Correlation Matrix')
-
-test_df = df[(df.event_key == 'dude_fixed_building')]
-
-
-exp.histogram(test_df.time, 'Buildings Fixed Over Time', 30, (0, 300000))
-
-exp.time_animate_graph(exp.location_heatmap, test_df, 'pos_x', 'pos_y', 'Heatmap')
+# test_event_df = event_df[(event_df.event_key == 'dude_harvested_resource_tree')]
 
 
+# exp.histogram(test_df.time, 'Buildings Fixed Over Time', 30, (0, 300000))
 
+# exp.time_animate_graph(exp.location_heatmap, test_event_df, 'pos_x', 'pos_y', 'Heatmap of Trees Harvested')
 
 
 # # Display an event plot and bar chart for age
